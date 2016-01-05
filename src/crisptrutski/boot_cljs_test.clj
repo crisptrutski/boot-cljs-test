95e6a2d23579c9c554798f372a9e62c764d9e5b6
Juho Teperi <juho.teperi@iki.fi>
2016-01-06 01:36:38 +0200

    Create cljs.edn which only loads test suite ns
    Fixes #33

--- src/crisptrutski/boot_cljs_test.clj
+++ src/crisptrutski/boot_cljs_test.clj
@@ -61,11 +61,15 @@
                                generated."]
   (assert (or (nil? namespaces) (set? namespaces)) "Namespaces should be a set")
   (let [out-file (or out-file default-output)
+        out-id   (str/replace out-file #"\.js$" "")
         suite-ns (or suite-ns default-suite-ns)
         tmp-main (core/tmp-dir!)]
     (core/with-pre-wrap fileset
       (let [namespaces (u/refine-namespaces fileset namespaces)]
         (core/empty-dir! tmp-main)
+        (info "Writing %s...\n" (str out-id ".cljs.edn"))
+        (spit (doto (io/file tmp-main (str out-id ".cljs.edn")) io/make-parents)
+              (pr-str {:require [suite-ns]}))
         (add-suite-ns! fileset tmp-main suite-ns namespaces)))))
 
 (deftask run-cljs-tests
