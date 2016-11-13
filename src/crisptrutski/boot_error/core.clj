ec0d68668c157f04149b62ca4f5b684c0418d711
Chris Truter <chris.truter@yuppiechef.com>
2016-11-13 15:28:13 +0200

    Error smuggling util upgrade

--- src/crisptrutski/boot_error/core.clj
+++ src/crisptrutski/boot_error/core.clj
@@ -1,7 +1,5 @@
 (ns crisptrutski.boot-error.core)
 
-(def ^:dynamic *errs* (atom nil))
-
 (defn- conjv
   "Push item onto end of collection. Coerces to vector so that subsequent appends are cheap."
   [xs x]
@@ -10,24 +8,24 @@
 (defn track-error
   "Record an error as fileset metadata"
   [fs error]
-  (with-meta fs (update (meta fs) :errors #(conjv % error))))
+  (with-meta fs (update (meta fs) ::errors #(conjv % error))))
 
 (defn track-errors
   "Record errors as fileset metadata"
   [fs errors]
-  (with-meta fs (update (meta fs) :errors #(into (vec %) errors))))
+  (with-meta fs (update (meta fs) ::errors #(into (vec %) errors))))
 
-(defn flush-errors!
-  "Record all errors in dynamic scope to filset metadata."
+(defn clear-errors
+  "Remove errors from fileset"
   [fs]
-  (let [errs @*errs*]
-    (reset! *errs* nil)
-    (reduce track-error fs errs)))
+  (with-meta fs (dissoc (meta fs) ::errors)))
 
 (defn get-errors
   "Read back all errors recorded in fileset metadata."
   [fs]
-  (:errors (meta fs)))
+  (::errors (meta fs)))
+
+(def ^:dynamic *errs* (atom nil))
 
 (defn track-error!
   "Record an error in dynamic scope"
@@ -38,4 +36,7 @@
   "Flush all errors captured within given body and record in result metadata."
   [& body]
   `(binding [*errs* (atom nil)]
-     (flush-errors! (do ~@body))))
+     (let [fs# (do ~@body)
+           errs# @*errs*]
+       (reset! *errs* nil)
+       (track-errors fs# errs#))))
